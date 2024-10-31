a568f502e7a6ddecdd10be9d18165490da5aa163
Sergey Opekunov <sergos5@yandex.ru>
2024-10-31 18:31:04 +0300

    add: slider

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
-console.log('hello, world');
+const sliderBlock = document.querySelector('.work-block')
+const slideItems = sliderBlock.querySelectorAll('.work-img')
+const dotsBlock = sliderBlock.querySelector('.work-dots')
+
+let dots
+let counter = 0;
+
+const addDots = () => {
+    for (let i = 0; i < slideItems.length; i++) {
+        const li = document.createElement('li');
+        li.classList.add('dot');
+        dotsBlock.append(li);
+    }
+    dots = dotsBlock.querySelectorAll('.dot');
+    dots[0].classList.add('dot-active');
+};
+
+const autoSlide = () => {
+    slideItems[counter].classList.remove('work-img-active')
+    dots[counter].classList.remove('dot-active')
+    counter++
+    if(counter>=slideItems.length) {
+        counter=0        
+    }  
+    slideItems[counter].classList.add('work-img-active')
+    dots[counter].classList.add('dot-active')
+}
+
+addDots()
+setInterval(()=>{
+    autoSlide()      
+}, 2500)
+
